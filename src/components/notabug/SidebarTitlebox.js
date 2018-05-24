802eef67ed701d97cd7fdebcd68ca8e0cac5889a
go1dfish <me@go1dfish.me>
2018-05-24 12:03:02 -0700

    Show version numbers in sidebar.  upgrade gun

--- src/components/notabug/SidebarTitlebox.js
+++ src/components/notabug/SidebarTitlebox.js
@@ -3,13 +3,17 @@ import README from "!raw-loader!README.md";
 import React from "react";
 import { SidebarTitlebox as SnewSidebarTitlebox } from "snew-classic-ui";
 import { Markdown } from "./Markdown";
+import { Timestamp } from "./Timestamp";
 
 export const SidebarTitlebox = (props) => (
   <div>
     <SnewSidebarTitlebox
       {...props}
       Markdown={Markdown}
-      description={README}
+      Timestamp={Timestamp}
+      description={README + `\n\n---\nnotabug v${process.env.REACT_APP_VERSION}`}
+      created={(new Date())}
+      created_utc={(new Date())}
     />
   </div>
 );
