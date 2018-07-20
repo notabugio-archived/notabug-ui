ab0d04b81c2e112884f715e9bcea492df3df46db
go1dfish <me@go1dfish.me>
2018-07-20 18:07:39 +0000

    Ignore server-build for babelify

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || "production";
 require("ignore-styles");
 
 require("babel-register")({
-  ignore: [ /(node_modules)/ ],
+  ignore: [ /(node_modules|server-build)/ ],
   presets: ["es2015", "react-app"]
 });
 
