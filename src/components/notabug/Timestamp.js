88afd70ec245fc2fe95d99b42c800e5ddff68a7c
go1dfish <me@go1dfish.me>
2018-08-25 17:44:59 +0000

    Timestamp component performance improvement

--- src/components/notabug/Timestamp.js
+++ src/components/notabug/Timestamp.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import TimeAgo from "react-timeago";
 
-export const Timestamp = ({ created_utc }) => (created_utc && <TimeAgo date={created_utc * 1000} />) || null;
+class PureTimestamp extends PureComponent {
+  render () {
+    const { created_utc } = this.props;
+    return (created_utc && <TimeAgo date={created_utc * 1000} />) || null;
+  }
+}
+
+export const Timestamp = ({ created_utc }) => <PureTimestamp created_utc={created_utc} />;
