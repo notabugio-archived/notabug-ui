4b4fdae2d561d90d736a6a6793d0869cd413743f
go1dfish <me@go1dfish.me>
2018-11-21 21:43:54 -0800

    Voting status tracks scroll

--- src/Voting/SidebarVotingStatus.js
+++ src/Voting/SidebarVotingStatus.js
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { VotingQueueContext } from "./hooks";
 import Spinner from "react-spinkit";
+import VisibilitySensor from "react-visibility-sensor";
 
-export const SidebarVotingStatus = ({ color = "#cee3f8" }) => {
+export const SidebarVotingStatus = React.memo(({ color = "#FF8B60" }) => {
   const {
     voteQueue,
     currentVote,
@@ -12,48 +13,58 @@ export const SidebarVotingStatus = ({ color = "#cee3f8" }) => {
   } = useContext(VotingQueueContext);
   const votingCount = Object.keys(voteQueue).length;
   if (!votingCount) return null;
-  return (
-    <div className="spacer">
-      <div className="sidecontentbox">
-        <div className="title">
+  const [isVisible, setIsVisible] = useState(true);
+
+  const content = (
+    <React.Fragment>
+      <div className="title">
+        {currentVote ? (
+          <h1>
+            CPU Voting on {votingCount} item{votingCount === 1 ? "" : "s"}
+          </h1>
+        ) : (
+          <h1>
+            CPU Voting Paused ({votingCount} item
+            {votingCount === 1 ? "" : "s"})
+          </h1>
+        )}
+      </div>
+      <div className="content">
+        {currentVote ? (
+          <Spinner
+            style={{ float: "left" }}
+            name="three-bounce"
+            fadeIn="none"
+            color={color}
+          />
+        ) : null}
+        <div className="more">
           {currentVote ? (
-            <h1>
-              CPU Voting on {votingCount} item{votingCount === 1 ? "" : "s"}
-            </h1>
-          ) : (
-            <h1>
-              CPU Voting Paused ({votingCount} item
-              {votingCount === 1 ? "" : "s"})
-            </h1>
-          )}
-        </div>
-        <div className="content">
-          <p>Votes use Proof of Work without cryptocurrency</p>
-          {currentVote ? (
-            <Spinner
-              style={{ float: "left" }}
-              name="three-bounce"
-              fadeIn="none"
-              color={color}
-            />
-          ) : null}
-          <div className="more">
-            {currentVote ? (
-              <a href="" onClick={onPauseQueue}>
-                pause
-              </a>
-            ) : (
-              <a href="" onClick={onResumeQueue}>
-                resume
-              </a>
-            )}{" "}
-            |{" "}
-            <a href="" onClick={onResetQueue}>
-              cancel
+            <a href="" onClick={onPauseQueue}>
+              pause
             </a>
-          </div>
+          ) : (
+            <a href="" onClick={onResumeQueue}>
+              resume
+            </a>
+          )}{" "}
+          |{" "}
+          <a href="" onClick={onResetQueue}>
+            cancel
+          </a>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
-};
+
+  return (
+    <VisibilitySensor onChange={setIsVisible}>
+      <div className="spacer">
+        <div className="sidecontentbox">{content}</div>
+        {isVisible ? null : (
+          <div className="sidecontentbox vote-status-fixed">{content}</div>
+        )}
+      </div>
+    </VisibilitySensor>
+  );
+});
