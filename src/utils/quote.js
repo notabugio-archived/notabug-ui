49f9655ce0ecfbfdf13e2cb853e7dac5b0005149
Grump <megagrump@protonmail.com>
2019-10-04 02:38:28 +0200

    quote chat messages

---
+++ src/utils/quote.js
@@ -0,0 +1,11 @@
+const quote = (text) => {
+	if(text.length == 0)
+		return text
+
+	text = text
+		.replace(/\n\s*\n/g, "\n")  // condense newlines
+		.replace(/\n(?!$)/g, "\n>") // quote all lines, no quote after last newline
+	return ">" + text
+}
+
+export default quote
